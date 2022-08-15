ff0bca92e003e004496b457edc150636277549a1
K.G <100962601+KBGR55@users.noreply.github.com>
2022-08-14 22:09:34 -0500

    Actualización versión 5.05 - vista

--- RED-EMART/src/vista/PanelNuevoBienInmueble.java
+++ RED-EMART/src/vista/PanelNuevoBienInmueble.java
@@ -7,8 +7,6 @@ package vista;
 import controlador.CaptadorController.RegistrarInmuebles;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import static vista.InterfacePrincipal.Panelcontenido;
@@ -18,6 +16,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
     private boolean edition;
     String origId;
     RegistrarInmuebles registro = new RegistrarInmuebles();
+    Integer bien;
 
     public PanelNuevoBienInmueble() {
         initComponents();
@@ -25,6 +24,31 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         Text6.setVisible(false);
         estadoComboBox.setVisible(false);
         edition = false;
+    }
+
+    public PanelNuevoBienInmueble(Integer bien, String dir, String des, String precio) {
+        initComponents();
+        llenarTabla();
+        this.edition = true;
+        Title.setText("Editar Bien Inmueble");
+        Text6.setVisible(true);
+        Text11.setVisible(false);
+        txtCedula.setVisible(false);
+        jSeparator21.setVisible(false);
+        estadoComboBox.setVisible(true);
+        jLabel1.setText("Editar");
+        direccionTxT.setText(dir);
+        descripcionTxT.setText(des);
+        precioTxT1.setText(precio);
+        this.bien = bien;
+    }
+
+    public void limpiar() {
+        txtCedula.setText("Ingresar cedula captador");
+        descripcionTxT.setText("Seleccionar ID descripcion");
+        direccionTxT.setText("Seleccionar ID direccion");
+        precioTxT1.setText("Ingrese el precio");
+
     }
 
     public void mostrarContenido(JPanel p) {
@@ -39,6 +63,55 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
     public void llenarTabla() {
         registro.leerDesc(tblDescripcion, registro.getDaoDes().listar());
         registro.leerDir(tblDireccion, registro.getDaoD().listar());
+    }
+
+    public void guardar() throws Exception {
+        if (txtCedula.getText().equalsIgnoreCase("Ingresar cedula captador") || txtCedula.getText().trim().length() == 0 || precioTxT1.getText().trim().length() == 0 || precioTxT1.getText().equalsIgnoreCase("Ingrese el precio") || direccionTxT.getText().equalsIgnoreCase("Seleccionar ID direccion") || direccionTxT.getText().trim().length() == 0 || descripcionTxT.getText().equalsIgnoreCase("Seleccionar ID descripcion") || descripcionTxT.getText().trim().length() == 0) {
+            JOptionPane.showMessageDialog(null, "Datos incompletos", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } else {
+            registro.registrarInmueble(Integer.valueOf(descripcionTxT.getText()), Integer.valueOf(direccionTxT.getText()), DateFecha.getDate(), Integer.valueOf(registro.id_captador(txtCedula.getText())), Double.valueOf(precioTxT1.getText()), "EB_D");
+            JOptionPane.showMessageDialog(null, "Datos ingresados correctamente", "ERROR", JOptionPane.ERROR_MESSAGE);
+            limpiar();
+        }
+    }
+
+    public void editar() throws Exception {
+        System.out.println("vista.PanelNuevoBienInmueble.editar()");
+        if (txtCedula.getText().trim().length() == 0 || precioTxT1.getText().trim().length() == 0 || direccionTxT.getText().trim().length() == 0 || descripcionTxT.getText().trim().length() == 0) {
+            JOptionPane.showMessageDialog(null, "Datos incompletos", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } else {
+            Boolean correct = false;
+            int tipoBien = estadoComboBox.getSelectedIndex();
+            try {
+                switch (tipoBien) {
+                    case 1:
+
+                        //correct = registro.modificarInmueble(bien, Integer.valueOf(descripcionTxT.getText()), Integer.valueOf(direccionTxT.getText()), DateFecha.getDate(), Integer.valueOf(registro.id_captador(txtCedula.getText())), Double.valueOf(precioTxT1.getText()), "EE_D");
+                        break;
+                    case 2:
+                        System.out.println(bien);
+                        System.out.println(descripcionTxT.getText());
+                        System.out.println(descripcionTxT.getText());
+                        System.out.println(descripcionTxT.getText());
+                        correct = registro.modificarInmueble(bien, Integer.valueOf(descripcionTxT.getText()),
+                                Integer.valueOf(direccionTxT.getText()),
+                                DateFecha.getDate(),
+                                Double.valueOf(precioTxT1.getText()), "EE_V");
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Seleccione el estado del inmueble", "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Datos erroneos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            if (correct) {
+                JOptionPane.showMessageDialog(null, "Datos modificados correctamente", "Modificar", JOptionPane.INFORMATION_MESSAGE);
+                //limpiar();
+            } else {
+                JOptionPane.showMessageDialog(null, "No se pudo modificar los datos ingresados", "Modificar", JOptionPane.ERROR);
+            }
+        }
     }
 
     private void cargarVista() {
@@ -70,15 +143,6 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
                 JOptionPane.showMessageDialog(null, "ERROR");
             }
         }
-    }
-
-    public PanelNuevoBienInmueble(Boolean edition) {
-        initComponents();
-        this.edition = edition;
-        Title.setText("Editar Bien Inmueble");
-        Text6.setVisible(true);
-        estadoComboBox.setVisible(true);
-        jLabel1.setText("Guardar");
     }
 
     private void setColor(JPanel p) {
@@ -118,6 +182,9 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         Text17 = new javax.swing.JLabel();
         jSeparator20 = new javax.swing.JSeparator();
         DateFecha = new com.toedter.calendar.JDateChooser();
+        Text11 = new javax.swing.JLabel();
+        txtCedula = new javax.swing.JTextField();
+        jSeparator21 = new javax.swing.JSeparator();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setMinimumSize(new java.awt.Dimension(750, 430));
@@ -156,9 +223,9 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Subir");
-        BotonGuardar.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, -1, 30));
+        BotonGuardar.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 0, -1, 30));
 
-        add(BotonGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 330, 180, 50));
+        add(BotonGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 350, 210, 30));
 
         Text6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         Text6.setText("Estado del Bien Inmueble");
@@ -173,6 +240,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         add(Text16, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 140, -1, -1));
 
         descripcionTxT.setEditable(false);
+        descripcionTxT.setBackground(new java.awt.Color(255, 255, 255));
         descripcionTxT.setForeground(new java.awt.Color(102, 102, 102));
         descripcionTxT.setText("Seleccionar ID descripcion");
         descripcionTxT.setBorder(null);
@@ -192,7 +260,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
 
         Text8.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         Text8.setText("TABLA DE DIRECCIÓN");
-        add(Text8, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 230, 170, -1));
+        add(Text8, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 190, 170, -1));
 
         precioTxT1.setForeground(new java.awt.Color(102, 102, 102));
         precioTxT1.setText("Ingrese el precio");
@@ -209,6 +277,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         add(jSeparator18, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 190, 260, 10));
 
         direccionTxT.setEditable(false);
+        direccionTxT.setBackground(new java.awt.Color(255, 255, 255));
         direccionTxT.setForeground(new java.awt.Color(102, 102, 102));
         direccionTxT.setText("Seleccionar ID direccion");
         direccionTxT.setBorder(null);
@@ -254,7 +323,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         });
         jScrollPane1.setViewportView(tblDireccion);
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 300, 100));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 300, 100));
 
         tblDescripcion.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -282,7 +351,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         });
         jScrollPane2.setViewportView(tblDescripcion);
 
-        add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 300, 100));
+        add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, 300, 100));
 
         Text9.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         Text9.setText("Dirección");
@@ -290,7 +359,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
 
         Text10.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         Text10.setText("TABLA DE DESCRIPCIÓN");
-        add(Text10, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 80, 170, -1));
+        add(Text10, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 60, 170, -1));
 
         Text17.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         Text17.setText("Fecha");
@@ -302,6 +371,29 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
 
         DateFecha.setDateFormatString("yyyy-MM-dd");
         add(DateFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 220, 260, 30));
+
+        Text11.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        Text11.setText("Cedula");
+        add(Text11, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 320, 130, -1));
+
+        txtCedula.setForeground(new java.awt.Color(102, 102, 102));
+        txtCedula.setText("Ingresar cedula captador");
+        txtCedula.setBorder(null);
+        txtCedula.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                txtCedulaMousePressed(evt);
+            }
+        });
+        txtCedula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCedulaActionPerformed(evt);
+            }
+        });
+        add(txtCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 340, 300, 30));
+
+        jSeparator21.setForeground(new java.awt.Color(0, 153, 255));
+        jSeparator21.setPreferredSize(new java.awt.Dimension(200, 10));
+        add(jSeparator21, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 370, 300, 10));
     }// </editor-fold>//GEN-END:initComponents
 
     private void BotonGuardarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonGuardarMouseEntered
@@ -314,20 +406,21 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
 
     // SUBIR
     private void BotonGuardarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonGuardarMousePressed
-        try {
-            guardar();
-        } catch (Exception ex) {
-            Logger.getLogger(PanelNuevoBienInmueble.class.getName()).log(Level.SEVERE, null, ex);
+        if (this.edition == false) {
+            try {
+                guardar();
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        } else {
+            try {
+                editar();
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
         }
     }//GEN-LAST:event_BotonGuardarMousePressed
-    
-    public void guardar() throws Exception {
-        if (precioTxT1.getText().trim().length() == 0 || direccionTxT.getText().equalsIgnoreCase("Seleccionar ID direccion") || direccionTxT.getText().equalsIgnoreCase("Seleccionar ID descripcion")) {
-            JOptionPane.showMessageDialog(null, "Datos incompletos", "ERROR", JOptionPane.ERROR_MESSAGE);
-        } else {
-            registro.registrarInmueble(Integer.valueOf(descripcionTxT.getText()), Integer.valueOf(direccionTxT.getText()),DateFecha.getDate(), 1,Double.valueOf(precioTxT1.getText()) ,"EB_D" );
-        }   
-    }
+
 
     private void descripcionTxTMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_descripcionTxTMousePressed
 
@@ -342,9 +435,7 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
     }//GEN-LAST:event_direccionTxTMousePressed
 
     private void tblDescripcionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDescripcionMouseClicked
-
         cargarVista();
-
     }//GEN-LAST:event_tblDescripcionMouseClicked
 
     private void tblDireccionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDireccionMouseClicked
@@ -355,10 +446,19 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_direccionTxTActionPerformed
 
+    private void txtCedulaMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtCedulaMousePressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCedulaMousePressed
+
+    private void txtCedulaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCedulaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCedulaActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BotonGuardar;
     private com.toedter.calendar.JDateChooser DateFecha;
     private javax.swing.JLabel Text10;
+    private javax.swing.JLabel Text11;
     private javax.swing.JLabel Text16;
     private javax.swing.JLabel Text17;
     private javax.swing.JLabel Text6;
@@ -377,9 +477,11 @@ public class PanelNuevoBienInmueble extends javax.swing.JPanel {
     private javax.swing.JSeparator jSeparator18;
     private javax.swing.JSeparator jSeparator19;
     private javax.swing.JSeparator jSeparator20;
+    private javax.swing.JSeparator jSeparator21;
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JTextField precioTxT1;
     private javax.swing.JTable tblDescripcion;
     private javax.swing.JTable tblDireccion;
+    private javax.swing.JTextField txtCedula;
     // End of variables declaration//GEN-END:variables
 }
